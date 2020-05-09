959c1189de9cadb5d22b522d311c8404b7dd79ee
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2020-05-09 20:31:33 +0200

    Added graph representation

--- src/Graph.py
+++ src/Graph.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import requests
 
+# some names of countries are diffrent in covid dataset and in borders data
 mapping_countries_names = {
     'Bonaire, Saint Eustatius and Saba' : 'Netherlands',
     'Brunei_Darussalam' : 'Brunei',
@@ -19,6 +21,8 @@ mapping_countries_names = {
 }
 
 class Graph:
+    """Represents structure of Countries with borders and data about COVID in regions"""
+
     def __init__(self, borders, cases):
         self.edges = borders
         self.vertices = cases
@@ -30,4 +34,29 @@ class Graph:
             return self.edges[country.upper()]
 
     def get_stats(self, country, date):
-        None
+        return self.vertices.loc[country.upper(), date.strftime('%d/%m/%Y')]
+
+
+def refresh_covid_data():
+    """Updates data about covid in local repository"""
+
+    COVID_URL = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+    r = requests.get(COVID_URL, allow_redirects=True)
+    open("../data/covid_data.csv", "wb").write(r.content)
+
+def get_graph():
+    """Returns graph containing borders between countries and covid data"""
+
+    cases = pd.read_csv("../data/covid_data.csv")
+    cases['countriesAndTerritories'] = cases['countriesAndTerritories'].map(str.upper)
+    cases = cases.set_index(['countriesAndTerritories', 'dateRep'])
+
+    with open("../data/borders.csv", mode="r", encoding="utf-8-sig") as file:
+        borders = {}
+        for line in file:
+            neighbours = line.replace('\n', '').replace(' ', '_').upper().split(',')
+            borders[neighbours[0]] = neighbours[1:]
+        file.close()
+
+    graph = Graph(borders, cases)
+    return graph
